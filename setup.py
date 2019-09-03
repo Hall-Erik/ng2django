aa8745f11654396c16879707e854c44e9761da27
Erik <hall.erik@gmail.com>
2019-09-03 15:59:10 -0600

    restructure

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'ng2django',
   scripts = ['ng2django'],
-  version = '0.1.1',
+  version = '0.1.2',
   license='MIT',
   description = 'Convert your Angular built index.html file to Django Template syntax',
   author = 'Erik Hall',
   author_email = 'hall.erik@gmail.com',
   url = 'https://github.com/Hall-Erik/ng2django',
-  download_url = 'https://github.com/Hall-Erik/ng2django/archive/v_01_1.tar.gz',
+  download_url = 'https://github.com/Hall-Erik/ng2django/archive/v_01_2.tar.gz',
   keywords = ['Django', 'Angular'],
   install_requires=[            # I get to this in a second
           'beautifulsoup4',
@@ -19,7 +19,6 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
   ],
